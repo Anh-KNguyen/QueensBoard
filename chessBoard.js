70528d37954d86ef5eb71384a9717d969202aa73
Annnh3 <anhknguyen97@yahoo.com>
2020-02-20 16:43:15 -0800

    Clean up CSS

--- chessBoard.js
+++ chessBoard.js
@@ -1,8 +1,19 @@
 var chessBoard = document.getElementById("chessBoard");
 
 for (var i = 0; i < 8; i++) {
-    var row = chessBoard.appendChild(document.createElement("div"));
+    var row = chessBoard.appendChild(document.createElement("div")); // row
     for (var j = 0; j < 8; j++) {
-        row.appendChild(document.createElement("span"));
+        var span = chessBoard.appendChild(document.createElement("span")); // column
+        if (i & 1) { // odd
+            if (j & 1) { // white
+            } else { //black
+                span.style.backgroundColor = "black";
+            }
+        } else { // even
+            if (j & 1) { // black
+                span.style.backgroundColor = "black";
+            }
+        }
+        row.appendChild(span)
     }
 }
